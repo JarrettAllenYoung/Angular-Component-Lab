56e939d990a1c5ee0599b30b923bc8bd4000bd81
Jarrett Allen Young <young.jarrett@gmail.com>
2026-01-04 15:24:20 -0800

    restyled login signup form component, added form to popup modal component

--- src/app/pages/components/tabs-login-signup-form/tabs-login-signup-form.page.ts
+++ src/app/pages/components/tabs-login-signup-form/tabs-login-signup-form.page.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TabsLoginSignupFormComponent } from '../../../components/tabs-login-signup-form/tabs-login-signup-form.component';
+import { PopupModalComponent } from '../../../components/popup-modal/popup-modal.component';
 
 @Component({
   standalone: true,
-  imports: [RouterLink, TabsLoginSignupFormComponent],
+  imports: [RouterLink, TabsLoginSignupFormComponent, PopupModalComponent],
   templateUrl: './tabs-login-signup-form.page.html',
   styleUrl: './tabs-login-signup-form.page.css',
 })
-export class TabsLoginSignupFormPageComponent {}
+export class TabsLoginSignupFormPageComponent {
+  isOpen = signal(false);
+
+  open() {
+    this.isOpen.set(true);
+  }
+
+  setOpen(next: boolean) {
+    this.isOpen.set(next);
+  }
+}
